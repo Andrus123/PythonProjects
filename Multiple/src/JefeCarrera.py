1ede2cc1a0f91001331cd02575bf5d8f8363e133
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-18 01:58:10 -0400

    Clase heredada: JefeCarrera, from Administrativo y from Docente

--- Multiple/src/JefeCarrera.py
+++ Multiple/src/JefeCarrera.py
@@ -5,5 +5,30 @@
 __author__ = "Andrés Aquin"
 __date__ = "$18-04-2021 01:19:40 AM$"
 
-class JefeCarrera():
+from Administrativo import Administrativo
+from Docente import Docente
+class JefeCarrera(Docente, Administrativo):
+    def __init__(self, categoria, tipo, item, fechaIngreso, unidad):
+        Docente.__init__(self, categoria, tipo)
+        Administrativo.__init__(self, item, fechaIngreso)
+        self.unidad=unidad
+        
+    def getUnidad(self):
+        return self.unidad
+    
+    def setUnidad(self, unidad):
+        self.unidad=unidad
+        
+    def leer(self):
+        Docente.leer(self)
+        Administrativo.leer(self)
+        print("Unidad: ")
+        u=input()
+        self.setUnidad(u)
+        
+    def mostrar(self):
+        Docente.mostrar(self)
+        Administrativo.mostrar(self)
+        print("Unidad: ", self.unidad)
+        
     
