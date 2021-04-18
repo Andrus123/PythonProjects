5b432d6d0da4e003bb77eb2cda2e2c9d776e7815
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-18 01:38:54 -0400

    Metodos regulares para Docente

--- Multiple/src/Docente.py
+++ Multiple/src/Docente.py
@@ -5,5 +5,31 @@
 __author__ = "Andrés Aquin"
 __date__ = "$18-04-2021 01:18:51 AM$"
 
-if __name__ == "__main__":
-    print "Hello World"
+class Docente():
+    def __init__(self, categoria, tipo):
+        self.categoria=categoria
+        self.tipo=tipo
+    
+    def getCategoria(self):
+        return self.categoria
+    
+    def getTipo(self):
+        return self.tipo
+    
+    def setCategoria(self, c):
+        self.categoria=c
+        
+    def setTipo(self, t):
+        self.tipo=t
+        
+    def leer(self):
+        self.setCategoria(input("Categoria: "))
+        print("Tipo: ")
+        tip=input()
+        self.setTipo(tip) #es el mismo metodo que categoria solo que más largo
+        
+    def mostrar(self):
+        print("Categoria: "+self.getCategoria())
+        print("Tipo: "+self.tipo)
+        
+        
