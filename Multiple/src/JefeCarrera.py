c46d9d8ef9d59b98323882df6ebdd257b06c3d8e
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-18 01:21:25 -0400

    Creando Clases para Herencia Multiple en Python

---
+++ Multiple/src/JefeCarrera.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Andrés Aquin"
+__date__ = "$18-04-2021 01:19:40 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
